1bedb7258a24bef87a377dd46bfd5c9abe7fa7b3
Juno Khuong Tran <80585663+mickeykhuonginmain2002@users.noreply.github.com>
2022-12-13 15:46:55 +0700

    Update fil1.py

--- Filter/fil1.py
+++ Filter/fil1.py
@@ -3,24 +3,24 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-#读取图像
+#Read image
 img = cv.imread('lena.png', 0)
 
-#傅里叶变换
+#FFT convert to frequency domain
 f = np.fft.fft2(img)
 fshift = np.fft.fftshift(f)
 
-#设置高通滤波器
+#DC
 rows, cols = img.shape
 crow,ccol = int(rows/2), int(cols/2)
 fshift[crow-30:crow+30, ccol-30:ccol+30] = 0
 
-#傅里叶逆变换
+#FFT inverse
 ishift = np.fft.ifftshift(fshift)
 iimg = np.fft.ifft2(ishift)
 iimg = np.abs(iimg)
 
-#显示原始图像和高通滤波处理图像
+#Convert image
 plt.subplot(121), plt.imshow(img, 'gray'), plt.title('Original Image')
 plt.axis('off')
 plt.subplot(122), plt.imshow(iimg, 'gray'), plt.title('Result Image')
